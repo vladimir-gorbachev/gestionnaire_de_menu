c00e33a0cceedde855dab3cfe6f08f29f00aceb9
vladimir-gorbachev <vladimir.gorbachev@laplateforme.io>
2025-01-28 12:09:11 +0100

    création du front pour l'index et pour la page recettes de chef - fichiers php + css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'Accueil</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="onglets">
+        
+        <a href="recettes_de_chef.php" class="onglet">
+            <img src="img/ONGLET DECOUVREZ.png" alt="Découvrez nos recettes de Chef.fes">
+        </a>
+
+        <a href="link2.php" class="onglet">
+            <img src="img/ONGLET CREEZ.png" alt="Créez vos propres recettes">
+        </a>
+
+        <a href="link3.php" class="onglet">
+            <img src="img/ONGLET ORGANISEZ.png" alt="Organisez vos menus">
+        </a>
+    </div>
+</body>
+</html>
