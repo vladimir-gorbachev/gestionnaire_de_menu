b01005691e4b45e01e4c0494f0adf14af78cf276
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2025-01-28 16:01:01 +0100

    Création du header et du footer, responsive à finir

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang=" ">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content=" ">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Noa Cengarle, Armelle Pouzioux, Vladimir Gorbachev">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/ecde10fa93.js" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="./cssA.css">
+    <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
+    <title>My Website</title>
+</head>
+<body>
+    <!-- inclusion de l'entête du site -->
+    <?php require_once(__DIR__ . "/header.php"); ?>
+
+    <!-- inclusion du bas de page du site -->
+    <?php require_once(__DIR__ . "/footer.php"); ?>
+</body>
+</html>
