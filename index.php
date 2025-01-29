be35816370f4b437e5cfa9eeac9111f8da41ba85
armelle-pouzioux <armelle.pouzioux@laplateforme.io>
2025-01-29 14:34:27 +0100

    Header et Footer finis responsives super top !

--- index.php
+++ index.php
@@ -9,13 +9,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/ecde10fa93.js" crossorigin="anonymous"></script>
     
-    <link rel="stylesheet" href="./cssA.css">
+    <link rel="stylesheet" href="./cssA.css?v=<?php echo time(); ?>">
     <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <title>My Website</title>
 </head>
 <body>
     <!-- inclusion de l'entête du site -->
     <?php require_once(__DIR__ . "/header.php"); ?>
+
+   
 
     <!-- inclusion du bas de page du site -->
     <?php require_once(__DIR__ . "/footer.php"); ?>
