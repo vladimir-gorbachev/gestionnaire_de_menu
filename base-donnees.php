a1daf6c28b5c2f39b6bceafe74c3f33168c0d8d1
noa-cen <noacengarle@gmail.com>
2025-02-01 13:26:13 +0100

    mise a jour connexion base de données pour plesk

--- base-donnees.php
+++ base-donnees.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=gestionnaire_de_menu;charset=utf8", "root");
+    $pdo = new PDO("mysql:host=localhost;dbname=gestionnaire_de_menu;charset=utf8", "utilisateur", "utilisateur123");
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (exception $e) {
